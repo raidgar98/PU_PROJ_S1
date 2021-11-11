476872a5de0c349d7ffb3cd0b849f9950bb724c7
raidgar98 <raidgar98@onet.pl>
2021-11-11 21:07:30 +0100

    ⚰️ 📝 removed useless argument

--- scrapper/scrapper/pages/filters/__init__.py
+++ scrapper/scrapper/pages/filters/__init__.py
@@ -1,7 +1,7 @@
 from scrapper.types import BrowserType
 from .car_filters import setup_car_filters_impl
 
-def setup_car_filters(*, driver : BrowserType, car_brand : str, car_model : str, car_generation : str = None, proceed : bool = True, **url_params) -> None:
+def setup_car_filters(*, driver : BrowserType, car_brand : str, car_model : str, car_generation : str = None, **url_params) -> None:
 	"""
 	Using given driver, fills form on page that allows filtering cars and clicks search button
 
@@ -13,9 +13,7 @@ def setup_car_filters(*, driver : BrowserType, car_brand : str, car_model : str,
 	:type car_model: str
 	:param car_generation: generation of cat to put in a text box, defaults to None
 	:type car_generation: str, optional
-	:param proceed: if set to True [default], click() will be performed on search button
-	:type proceed: bool, optional
 
 	:return: nothing
 	"""
-	return setup_car_filters_impl(driver=driver, car_brand=car_brand, car_model=car_model, car_generation=car_generation, proceed=proceed, **url_params)
+	return setup_car_filters_impl(driver=driver, car_brand=car_brand, car_model=car_model, car_generation=car_generation, **url_params)
--- scrapper/scrapper/pages/filters/car_filters.py
+++ scrapper/scrapper/pages/filters/car_filters.py
@@ -1,5 +1,4 @@
 from scrapper.types import BrowserType, not_empty, verify_types
 
 @verify_types(car_brand=not_empty, car_model=not_empty)
-def setup_car_filters_impl(*, driver : BrowserType, car_brand : str, car_model : str, car_generation : str = None, proceed : bool = True, **url_params) -> None:
-	pass
+def setup_car_filters_impl(*, driver : BrowserType, car_brand : str, car_model : str, car_generation : str = None, **url_params) -> None:

3abad5ddfab27528c969e8f720a117c05b2e5a21
raidgar98 <raidgar98@onet.pl>
2021-11-11 19:12:02 +0100

    🏷️ intrduced alias file

---
+++ scrapper/scrapper/types.py
@@ -0,0 +1,14 @@
+# Typing library imports
+from typing import Any, Union, _SpecialForm
+
+# Key imports
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebElement
+
+# Import Aliases
+from selenium.webdriver import Chrome as BrowserType
+
+# Type Aliases
+NullableString = Union[str, None]
+

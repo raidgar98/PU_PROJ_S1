f9ec536980340eaac5a2c1ecc6fa9c989b90ea9a
raidgar98 <raidgar98@onet.pl>
2021-11-14 00:48:47 +0100

    📝 added description of utilitiwes module

--- scrapper/scrapper/utilities/common.py
+++ scrapper/scrapper/utilities/common.py
@@ -16,18 +16,71 @@ from selenium.webdriver.remote.webelement import WebElement
 log = get_logger()
 
 def find_elements(driver: BrowserType, by: By, search: str) -> List[WebElement]:
+	"""
+	encapsulates driver.find_elements
+
+	:param driver: driver to use
+	:type driver: BrowserType
+	:param by: filter type
+	:type by: By
+	:param search: filter query
+	:type search: str
+	:return: list of matched WebElements
+	:rtype: List[WebElement]
+	"""
 	return driver.find_elements(by, search)
 
 
 def element_dictionary(driver: BrowserType, by: By, search: str, key_foo: LambdaType) -> Dict[Any, WebElement]:
+	"""
+	associates output from processing of WebElement (by given lambda) to that WebElement. This is generic function
+
+	:param driver: driver to use
+	:type driver: BrowserType
+	:param by: filter type
+	:type by: By
+	:param search: filter query
+	:type search: str
+	:param key_foo: output from this lambda will be used as key in dict, so it should be hashable
+	:type key_foo: LambdaType
+	:rtype: Dict[Any, WebElement]
+	"""
 	return {key_foo(x): x for x in find_elements(driver, by, search)}
 
 
 def attribiute_dictionary(driver: BrowserType, by: By, search: str, attr: str) -> Dict[str, WebElement]:
+	"""
+	specialization of element_dictionary, where lambda returns given attribiute
+
+	:param driver: driver to use
+	:type driver: BrowserType
+	:param by: filter type
+	:type by: By
+	:param search: filter query
+	:type search: str
+	:param attr: value under this attr. will be used as key in result dict
+	:type attr: str
+	:rtype: Dict[str, WebElement]
+	"""
 	return element_dictionary(driver, by, search, lambda x: x.get_attribute(attr))
 
 
-def filter_attribiute_dictionary(driver: BrowserType, by: By, search: str, attr: str, filter_input: Union[Callable, str]):
+def filter_attribiute_dictionary(driver: BrowserType, by: By, search: str, attr: str, filter_input: Union[Callable, str]) -> Dict[str, WebElement]:
+	"""
+	encapsulates attribiute_dictionary and additionally filters output
+
+	:param driver: driver to use
+	:type driver: BrowserType
+	:param by: filter type
+	:type by: By
+	:param search: filter query
+	:type search: str
+	:param attr: value under this attr. will be used as key in result dict
+	:type attr: str
+	:param filter_input: str will be used as regex input
+	:type filter_input: Union[Callable, str]
+	:rtype: Dict[str, WebElement]
+	"""
 	if isinstance(filter_input, str):
 		compiled_regex = compile(filter_input)
 
@@ -39,6 +92,16 @@ def filter_attribiute_dictionary(driver: BrowserType, by: By, search: str, attr:
 
 
 def safely_fill_input(element: WebElement, text: str, *, requires_confirmation: bool = True):
+	"""
+	fills given WebElement as safe as it is possible (shouldn't throw exception)
+
+	:param element: should be input field
+	:type element: WebElement
+	:param text: text to write
+	:type text: str
+	:param requires_confirmation: if set to False additonal confiramtion (ENTER) will not be performed, defaults to True
+	:type requires_confirmation: bool, optional
+	"""
 	assert element is not None
 	assert element.tag_name == 'input'
 
@@ -55,16 +118,40 @@ def safely_fill_input(element: WebElement, text: str, *, requires_confirmation: 
 
 
 def safely_click(element: WebElement):
+	"""
+	encapsulates clicking on button
+
+	:param element: should be button
+	:type element: WebElement
+	"""
 	assert element is not None
 	element.click()
 	sleep(1)
 
 
-def split_name_amount(line: str):
+def split_name_amount(line: str) -> Tuple[str, int]:
+	"""
+	splits string
+
+	:example: "Honda (2000)" -> ["Honda", 2000]
+	:param line: text to process
+	:type line: str
+	:return: splitted line
+	:rtype: Tuple[str, int]
+	"""
 	splitted = line.strip().rsplit('(')
 	return splitted[0].strip(), int(splitted[1].strip('()'))
 
 
-def split_name_amount_with_date_ranges(line: str):
+def split_name_amount_with_date_ranges(line: str) -> Tuple[str, int]:
+	"""
+	splits string
+
+	:example: "Honda (2015-2019) (2000)" -> ["Honda (2015-2019)", 2000]
+	:param line: text to process
+	:type line: str
+	:return: splitted line
+	:rtype: Tuple[str, int]
+	"""
 	result: Match = regex_match('([A-Za-z0-9]+) \(([0-9]+-([0-9]+)?)\) \(([0-9]+)\)', line)
 	return f'{result.group(1)} ({result.group(2)})', result.group(4)
--- scrapper/scrapper/utilities/presets.py
+++ scrapper/scrapper/utilities/presets.py
@@ -1,48 +1,108 @@
-from typing import Callable, Union
+from typing import Callable, Dict, List, Union
+
+from selenium.webdriver.remote.webelement import WebElement
 
 from scrapper.types import BrowserType, By
 from scrapper.utilities.common import attribiute_dictionary, element_dictionary, filter_attribiute_dictionary, find_elements
 
 
 class Preset:
+	"""
+	presets are helper classes that automates process of finding specific types on web page
+	"""
 	def __init__(self, driver: BrowserType, by: By, search: str):
+		"""
+		[protected] inititalizes preset
+
+		:param driver: driver to use
+		:type driver: BrowserType
+		:param by: filter type
+		:type by: By
+		:param search: filter query
+		:type search: str
+		"""
 		self.driver = driver
 		self.by = by
 		self.search = search
 
-	def find(self):
+	def find(self) -> List[WebElement]:
+		"""
+		finds WebElements of preset type
+
+		:rtype: List[WebElement]
+		"""
 		return find_elements(self.driver, self.by, self.search)
 
 
 class Links(Preset):
+	"""
+	represents <a href=""> tags
+	"""
 	def __init__(self, driver: BrowserType):
 		super().__init__(driver, By.TAG_NAME, 'a')
 		self.attr = 'href'
 
-	def href_dict(self):
+	def href_dict(self) -> Dict[str, WebElement]:
+		"""
+		returns map of links->WebElement
+
+		:rtype: Dict[str, WebElement]
+		"""
 		return attribiute_dictionary(self.driver, self.by, self.search, self.attr)
 
-	def href_dict(self, filter: Union[Callable, str]):
+	def href_dict(self, filter: Union[Callable, str]) -> Dict[str, WebElement]:
+		"""
+		returns filtered map of links->WebElement
+
+		:param filter: function to use while filtering
+		:type filter: Union[Callable, str]
+		:rtype: Dict[str, WebElement]
+		"""
 		return filter_attribiute_dictionary(self.driver, self.by, self.search, self.attr, filter)
 
 
 class Buttons(Preset):
+	"""
+	represents <button> tag
+	"""
 	def __init__(self, driver: BrowserType):
 		super().__init__(driver, By.TAG_NAME, 'button')
 
-	def attr_filter(self, attr: str, filter: Union[Callable, str]):
+	def attr_filter(self, attr: str, filter: Union[Callable, str]) -> Dict[str, WebElement]:
+		"""
+		returns filtered map by any button tag attribute
+
+		:param attr: value under this attr. will be used as a key
+		:type attr: str
+		:param filter: function to use while filtering
+		:type filter: Union[Callable, str]
+		:rtype: Dict[str, WebElement]
+		"""
 		return filter_attribiute_dictionary(self.driver, self.by, self.search, attr, filter)
 
 
 class Inputs(Buttons):
+	"""
+	represents <input> tag
+
+	:note Buttons: it shares simillar functionalities, there is no logical connection
+	"""
 	def __init__(self, driver: BrowserType):
 		super().__init__(driver)
 		self.search = 'input'
 
 
 class Spans(Preset):
+	"""
+	represents <span> tag
+	"""
 	def __init__(self, driver, by: By = By.TAG_NAME, search: str = 'span'):
 		super().__init__(driver, by, search)
 
-	def content_dictionary(self):
+	def content_dictionary(self) -> Dict[str, WebElement]:
+		"""
+		returns map of span_content -> WebElement
+
+		:rtype: Dict[str, WebElement]
+		"""
 		return element_dictionary(self.driver, self.by, self.search, lambda x: x.text)

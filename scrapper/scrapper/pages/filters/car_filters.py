50403f8c97fc7c6880decb278df32de6d174f168
raidgar98 <raidgar98@onet.pl>
2021-11-13 23:08:43 +0100

    ♻️ refactored car_filters using new dev. tools

--- scrapper/scrapper/pages/filters/car_filters.py
+++ scrapper/scrapper/pages/filters/car_filters.py
@@ -2,8 +2,11 @@ from time import sleep
 from types import FunctionType
 from typing import List
 
-from scrapper import BASE_URL
+from scrapper.conf import BASE_URL
+from scrapper.pages.accessors.front_page import get_car_brands_input_field, get_car_generation_input_field, get_car_model_input_field
 from scrapper.types import BrowserType, not_empty, verify_types
+from scrapper.utilities.common import safely_click, safely_fill_input
+from scrapper.utilities.presets import Buttons, Inputs
 from scrapper.utilities.url_param_extender import update_url
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -12,37 +15,21 @@ from selenium.webdriver.remote.webelement import WebElement
 
 @verify_types(car_brand=not_empty, car_model=not_empty)
 def setup_car_filters_impl(*, driver: BrowserType, car_brand: str, car_model: str, car_generation: str = None, **url_params) -> None:
-	PLACEHOLDER = 'placeholder'		# placeholder caption
-	INPUT_FIELDS_PLACEHOLDERS = {		# unique strings to find on page
-		'Marka pojazdu': car_brand,
-		'Model pojazdu': car_model,
-		'Generacja': car_generation
-	}
 
-	if driver.current_url != BASE_URL:
-		driver.get(BASE_URL)
+	# fill proper inputs
+	safely_fill_input(get_car_brands_input_field(driver), car_brand)
+	safely_fill_input(get_car_model_input_field(driver, switch_tab=False), car_model)
+	if car_generation is not None:
+		safely_fill_input(get_car_generation_input_field(driver, switch_tab=False), car_generation)
 
-	# iterate over found text inputs
-	for input_element in find_matching_elements(driver, 'input', lambda x: x.get_attribute(PLACEHOLDER) in INPUT_FIELDS_PLACEHOLDERS):
-		placeholder = INPUT_FIELDS_PLACEHOLDERS[input_element.get_attribute(PLACEHOLDER)]
-		if placeholder is not None:
-			input_element.send_keys(placeholder)
-			# if not send, page will not activate next field
-			input_element.send_keys(Keys.ENTER)
-			sleep(1)
-
-	# find matching button
-	buttons = find_matching_elements(driver, 'button', lambda x: x.get_attribute('data-testid') == 'submit-btn')
+	# get submit button
+	buttons = Buttons(driver).attr_filter('data-testid', 'submit-btn')
 	assert len(buttons) == 1
-	buttons[0].click()
-	sleep(1)
+	safely_click(buttons[0])
 
 	# add to current url additional parameters
 	if len(url_params) > 0:
 		url = update_url(driver.current_url, **url_params)
-		driver.get(url)
 
-
-def find_matching_elements(driver: BrowserType, tag_name: str, filter: FunctionType) -> List[WebElement]:
-	elements = driver.find_elements(By.TAG_NAME, tag_name)
-	return [element for element in elements if filter(element)]
+	# apply filter (submit)
+	driver.get(url)

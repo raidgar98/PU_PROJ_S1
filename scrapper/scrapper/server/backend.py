80aa670c5d4f23ba75056522a9b32daad2af291a
raidgar98 <raidgar98@onet.pl>
2021-11-13 23:13:27 +0100

    ✨ implemented three new endpoints

--- scrapper/scrapper/server/backend.py
+++ scrapper/scrapper/server/backend.py
@@ -1,5 +1,6 @@
 from scrapper.conf import get_logger
 from scrapper.types import BrowserType
+from scrapper.pages.accessors.front_page import get_avaiable_car_brands, get_avaiable_car_models, get_avaiable_car_generations
 
 log = get_logger()
 
@@ -18,9 +19,15 @@ class BrowserInstance:
 		self.__driver.quit()
 
 	# cars
-	def get_car_brands(self): pass
-	def get_car_models(self, brand: str): pass
-	def get_car_generations(self, brand: str, model: str): pass
+	def get_car_brands(self):
+		return get_avaiable_car_brands(self.__driver)
+
+	def get_car_models(self, brand: str):
+		return get_avaiable_car_models(self.__driver, brand)
+
+	def get_car_generations(self, brand: str, model: str):
+		return get_avaiable_car_generations(self.__driver, brand, model)
+
 	def list_cars(self, brand: str, model: str, generation: str): pass
 	def get_car(self, link: str): pass
 
--- scrapper/scrapper/server/endpoints.py
+++ scrapper/scrapper/server/endpoints.py
@@ -7,11 +7,23 @@ from scrapper.types import verify_types
 
 
 @verify_types()
-def get_brands(ctx: BrowserInstance) -> List[str]:
-	return Success({"brands": ctx.get_car_brands()})
+def get_car_brands(ctx: BrowserInstance) -> List[str]:
+	return Success(ctx.get_car_brands())
+
+
+@verify_types()
+def get_car_models(ctx: BrowserInstance, *, brand: str) -> List[str]:
+	return Success(ctx.get_car_models(brand=brand))
+
+
+@verify_types()
+def get_car_generations(ctx: BrowserInstance, *, brand: str, model: str) -> List[str]:
+	return Success(ctx.get_car_generations(brand=brand, model=model))
 
 
 def build_methods() -> Methods:
 	return {
-		"cars.get_brands": get_brands
+		"cars.get_brands": get_car_brands,
+		"cars.get_models": get_car_models,
+		"cars.get_generations": get_car_generations,
 	}

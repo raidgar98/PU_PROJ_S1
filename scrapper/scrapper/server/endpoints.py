68092d0b1ca4eeb7d1490b85bdf8302ac4d5ca7f
raidgar98 <raidgar98@onet.pl>
2021-12-04 17:30:41 +0100

    ✨ added car offers listing to API
    Co-authored-by: DawidLechowskyy977 <dawilec197@student.polsl.pl>

--- scrapper/scrapper/server/endpoints.py
+++ scrapper/scrapper/server/endpoints.py
@@ -20,10 +20,15 @@ def get_car_models(ctx: BrowserInstance, *, brand: str) -> List[str]:
 def get_car_generations(ctx: BrowserInstance, *, brand: str, model: str) -> List[str]:
 	return Success(ctx.get_car_generations(brand=brand, model=model))
 
+@verify_types()
+def list_car_offers(ctx: BrowserInstance, *, brand: str, model: str, price_to : int, price_from : int = 0, generation : str = None, page : int = None) -> List[str]:
+	return Success(ctx.list_cars(brand=brand, model=model, generation=generation, price_to=price_to, price_from=price_from, page=page))
+
 
 def build_methods() -> Methods:
 	return {
 		"cars.get_brands": get_car_brands,
 		"cars.get_models": get_car_models,
 		"cars.get_generations": get_car_generations,
+		"cars.list": list_car_offers
 	}

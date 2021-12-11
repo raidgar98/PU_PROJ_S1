0d488c258ae4cd0bbfd7732950b4322cd29ee510
raidgar98 <raidgar98@onet.pl>
2021-12-12 00:20:31 +0100

    ✅ added negative tests for listing

--- scrapper/tests/cars/negative_test.py
+++ scrapper/tests/cars/negative_test.py
@@ -1,4 +1,5 @@
 from tests.utils import Cars
+from pytest import raises
 
 def test_get_model_invalid_brand(cars_api : Cars):
 	assert len(cars_api.get_models(brand='aaaaaaaaaaaaaaaaaaaaaaaa')) == 0
@@ -6,4 +7,6 @@ def test_get_model_invalid_brand(cars_api : Cars):
 def test_get_generation_invalid_model(cars_api : Cars):
 	assert len(cars_api.get_generations(brand='Opel', model='bbbbbbbbbbbbbbb')) == 0
 
-
+def test_list_cars_negative_value(cars_api : Cars):
+	with raises(AssertionError):
+		cars_api.list(brand='', model='', price_to=-1)
